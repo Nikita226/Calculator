4cd4539368beafc3c2ba981c265485c1a0a8640f
Nikita226 <nsk35235@gmail.com>
2022-11-22 14:58:03 +0530

    added all files

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+// name of component should capital
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="/">Navbar</a>
+    </nav>
+  )
+}
